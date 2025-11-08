4eae3fe0c36727494db8634d51e1ee3e274457f1
magaud <magaud@unistra.fr>
2025-11-08 20:12:06 +0100

    a new working script to run benchmarks

--- run.sh
+++ run.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 ### user variables (to be tuned according to your own setting
-ALL="delaunayflip GeoCoq"
-ROCQDITTODIR=../rocq-ditto # the directory which contains rocq-ditto
-OUTPUT=_output
+ALL="delaunayflip" #ALL="delaunayflip GeoCoq stdpp"
+ROCQDITTODIR="/Users/magaud/rocq-ditto" # the directory which contains rocq-ditto
+OUTPUT=_xxx
 
-
+eval $(opam env --switch=$ROCQDITTODIR --set-switch)
 ### general variables (do not change!)
 WDIR=$(pwd)
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 cd $ROCQDITTODIR
+make build
 for i in $ALL;
 do
     echo "======================== running rocq-ditto on directory ${bold}$i${normal}... ========================"
-    #echo "$WDIR/$i"
-    #echo "$WDIR/$i$OUTPUT"
-    dune exec --profile=release rocq-ditto -- -i $WDIR/$i -o $WDIR/$i_$OUTPUT -t turn_into_oneliner -v
+    echo "$WDIR/$i"
+    echo "$WDIR/$i$OUTPUT"
+    dune exec --profile=release rocq-ditto -- -i $WDIR/$i -o $WDIR/$i$OUTPUT -t turn_into_oneliner -v --save-vo
     echo "======================== done running rocq-ditto, output in ${bold}$i$OUTPUT${normal}! ========================"
 done
 cd $WDIR
